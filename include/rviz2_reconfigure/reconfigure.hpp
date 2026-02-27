fbbdca336a965ae439555635f43109b9265467f8
shikishima-TasakiLab <160442065@ccalumni.meijo-u.ac.jp>
2026-02-27 16:11:34 +0000

    パラメータダイアログとエディタデリゲートの実装: 新しいParamDialogおよびParamEditorDelegateクラスを追加し、CMakeLists.txtに関連ファイルを追加

--- include/rviz2_reconfigure/reconfigure.hpp
+++ include/rviz2_reconfigure/reconfigure.hpp
@@ -13,21 +13,21 @@
 #include <yaml-cpp/yaml.h>
 
 #include <QCheckBox>
-#include <QDialog>
 #include <QFileDialog>
-#include <QLineEdit>
 #include <QPushButton>
 #include <QStyledItemDelegate>
 #include <QTreeWidget>
 
+#include "rviz2_reconfigure/param_dialog.hpp"
+#include "rviz2_reconfigure/delegate.hpp"
+
 #include "ui_reconfigure.h"
-#include "ui_param_dialog.h"
-#endif
+
+#endif // Q_MOC_RUN
 
 namespace Ui
 {
     class Reconfigure;
-    class ParamDialog;
 } // namespace Ui
 
 namespace rviz2_reconfigure
@@ -80,39 +80,6 @@ namespace rviz2_reconfigure
         std::vector<rclcpp::Client<rcl_interfaces::srv::SetParameters>::SharedPtr> set_params_clients_;
     };
     
-
-    class ParamDialog : public QDialog
-    {
-        Q_OBJECT
-    public:
-        explicit ParamDialog(rclcpp::Node::SharedPtr node_handle, const std::shared_ptr<rclcpp::Logger> &logger, rviz_common::Panel *parent = nullptr);
-        ~ParamDialog();
-        QList<QPair<QString, QString> > getCheckedParams() const;
-    
-    protected Q_SLOTS:
-        void refresh();
-        void onItemChanged(QTreeWidgetItem *item, int column);
-    protected:
-        void updateChildCheckState(QTreeWidgetItem *item, Qt::CheckState state);
-        void updateParentCheckState(QTreeWidgetItem *item);
-        QTreeWidgetItem* getOrCreateChild(QTreeWidgetItem *parent, const QString &name);
-
-    private:
-        rclcpp::Node::SharedPtr nh_;
-        Ui::ParamDialog *ui_;
-        std::shared_ptr<rclcpp::Logger> logger_;
-        std::vector<rclcpp::Client<rcl_interfaces::srv::ListParameters>::SharedPtr> list_params_clients_;
-    };
-
-
-    class ParamEditorDelegate : public QStyledItemDelegate
-    {
-        Q_OBJECT
-    public:
-        ParamEditorDelegate(QObject *parent = nullptr) : QStyledItemDelegate(parent) {}
-        QWidget* createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
-    };
-
 } // namespace rviz2_reconfigure
 
 #endif // RVIZ2_RECONFIGURE__RECONFIGURE_HPP_
